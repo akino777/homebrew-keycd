3a4d599b1be336d50babd4d74dd57af4e9b2583b
akino777 <a.tera32539@gmail.com>
2024-12-08 00:04:02 +0900

    first commit

---
+++ keycd.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Keycd < Formula
+  desc "A simple command line tool to quickly switch between directories."
+  homepage ""
+  url "https://github.com/akino777/keycd/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "ca6dc394d8674af66d91abc8d6b8f08f3fc00807ed8bad64b8cd2512ebaf9e4c"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test keycd`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
